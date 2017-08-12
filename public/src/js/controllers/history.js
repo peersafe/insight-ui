fc64df2861f05525d3f50b580484acc87c27fbdc
efdssdf <efdssdf@gmail.com>
2017-08-12 08:11:18 -0500

    修改导出

--- public/src/js/controllers/history.js
+++ public/src/js/controllers/history.js
@@ -16,12 +16,10 @@ angular.module('insight.history').controller('HistoryController',
 
       $scope.dateval = _formatTimestamp(new Date());
 
-    
+
     // $scope.historys = HistoryService.list();
     $scope.list = function () {
       HistoryService.get({}, function (res) {
-        // console.log('tp::',typeof res.data,'res', res.data)
-          res = {"code":0,"msg":"Success","data":[{"import_time":"20170811-145006","user":[{"id":"history.blacklist.20170811-145006.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-145006"}],"txsnum":2},{"import_time":"20170811-145219","user":[{"id":"history.blacklist.20170811-145219.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-145219"}],"txsnum":2},{"import_time":"20170811-165334","user":[{"id":"history.blacklist.20170811-165334.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-165334"}],"txsnum":2},{"import_time":"20170811-165341","user":[{"id":"history.blacklist.20170811-165341.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-165341"}],"txsnum":2},{"import_time":"20170811-183227","user":[{"id":"history.blacklist.20170811-183227.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-183227"}],"txsnum":2},{"import_time":"20170811-183427","user":[{"id":"history.blacklist.20170811-183427.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-183427"}],"txsnum":2},{"import_time":"20170811-183627","user":[{"id":"history.blacklist.20170811-183627.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-183627"}],"txsnum":2},{"import_time":"20170811-183827","user":[{"id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-183827"}],"txsnum":2},{"import_time":"20170811-184027","user":[{"id":"history.blacklist.20170811-184027.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-184027"}],"txsnum":2},{"import_time":"20170811-184227","user":[{"id":"history.blacklist.20170811-184227.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-184227"}],"txsnum":2},{"import_time":"20170811-184427","user":[{"id":"history.blacklist.20170811-184427.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-184427"}],"txsnum":2},{"import_time":"20170811-184627","user":[{"id":"history.blacklist.20170811-184627.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR","comment":"nkbjb","txsnum":2,"time":"20170811-184627"}],"txsnum":2}]}
         if (res.code === 0) {
           $scope.histories = res.data;
           console.log($scope.histories)
@@ -29,125 +27,6 @@ angular.module('insight.history').controller('HistoryController',
       });
     }
     $scope.list();
-    $scope.histories = [
-        {
-            "import_time":"20170811145006",
-            "user":[
-                {
-                    "id":"history.blacklist.20170811-145006.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-145006"
-                },
-                {
-                    "id":"history.blacklist.20170811-145219.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-145219"
-                },
-                {
-                    "id":"history.blacklist.20170811-165334.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-165334"
-                },
-                {
-                    "id":"history.blacklist.20170811-165341.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-165341"
-                },
-                {
-                    "id":"history.blacklist.20170811-183227.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183227"
-                },
-                {
-                    "id":"history.blacklist.20170811-183427.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183427"
-                }
-            ],
-            "txsnum":2,
-            "limitpage":5
-        },
-        {
-            "import_time":"20170811183827",
-            "user":[
-                {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }, {
-                    "id":"history.blacklist.20170811-183827.muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "addr":"muv8WgxDydvKB82JZhLcyX1dsmyAmkFKhR",
-                    "comment":"nkbjb",
-                    "txsnum":2,
-                    "time":"20170811-183827"
-                }
-            ],
-            "txsnum":2,
-            "limitpage":5
-        }
-    ]
 
     $scope.histories.forEach(function(history,index){
       $scope["datashow"+history.import_time]=true;
