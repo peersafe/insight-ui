f3444377b72ee1a970280a66c14d065a2ce09f54
efdssdf <efdssdf@gmail.com>
2017-08-15 04:25:50 -0500

    修改导出字段名

--- public/src/js/controllers/transactions.js
+++ public/src/js/controllers/transactions.js
@@ -210,9 +210,9 @@ function($scope, $rootScope, $routeParams, $location, Global, Transaction, Trans
         if(_Txdirection(tx)){
           _processTX(tx);
           $scope.txs.push(tx);
-          $scope.exceltxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),value:tx.valueOut + " BTC",confirmations:tx.confirmations});
+          $scope.exceltxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),value:tx.valueOut + " BTC",confirmations:tx.confirmations + "个确认数"});
           console.log(tx.time);
-          $scope.exceladdtxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),valueIn:tx.valueIn + " BTC",valueOut:tx.valueOut + " BTC",confirmations:tx.confirmations});
+          $scope.exceladdtxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),valueIn:tx.valueIn + " BTC",valueOut:tx.valueOut + " BTC",confirmations:tx.confirmations + "个确认数"});
         }
       }
      
@@ -229,8 +229,8 @@ function($scope, $rootScope, $routeParams, $location, Global, Transaction, Trans
              if(_Txdirection(tx)){
                 _processTX(tx);
                 $scope.txs.push(tx);
-                $scope.exceltxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),value:tx.valueOut + " BTC",confirmations:tx.confirmations});
-                $scope.exceladdtxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),valueIn:tx.valueIn + " BTC",valueOut:tx.valueOut + " BTC",confirmations:tx.confirmations});
+                $scope.exceltxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),value:tx.valueOut + " BTC",confirmations:tx.confirmations + "个确认数"});
+                $scope.exceladdtxs.push({hash:tx.txid,time:_formatTime(new Date(tx.time * 1000)),valueIn:tx.valueIn + " BTC",valueOut:tx.valueOut + " BTC",confirmations:tx.confirmations + "个确认数"});
              }
           }
       })
