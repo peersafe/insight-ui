662a27216b8fbca80a32951c76086e8c12059dab
caozhongzheng <caozhongzheng@peersafe.cn>
2017-08-18 17:32:05 +0800

    Fix:BTCB-42 fetch import records which has 0 blacklist transactions

--- public/js/main.js
+++ public/js/main.js
@@ -123,7 +123,7 @@ angular.module('insight.blacklists')
     $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
 }])
     .controller('BlacklistsController',
-  function ($scope, $rootScope, $routeParams, $http, Service, BlacklistService) {
+  function ($scope, $rootScope, $routeParams, $http, Service, BlacklistService, Address) {
     // $scope.global = Global;
     // $scope.loading = false;
 
@@ -154,12 +154,28 @@ angular.module('insight.blacklists')
     $scope.doAdd = function () {
       if (!$scope.newblacklist || !$scope.newblacklist.addr) {
         $scope.newblacklist = {addr:'地址不能为空'}
-      } else if ($scope.newblacklist.addr.length < 34
-      || $scope.newblacklist.addr.length > 35) {
-        // TODO 还需要验证地址的有效性
-        $scope.newblacklist.addr = '无效的地址'
+      // } else if ($scope.newblacklist.addr.length < 34
+      // || $scope.newblacklist.addr.length > 35) {
       } else {
-        $scope.saveBlacklist($scope.newblacklist);
+        // 验证地址的有效性
+        Address.get({
+            addrStr: $scope.newblacklist.addr
+          },
+          function(address) {
+            $scope.saveBlacklist($scope.newblacklist);
+          },
+          function(e) {
+            if (e.status === 400) {
+              // $rootScope.flashMessage = 'Invalid Address: ' + $routeParams.addrStr;
+              $scope.newblacklist.addr = '无效的地址'
+            } else if (e.status === 503) {
+              // $rootScope.flashMessage = 'Backend Error. ' + e.data;
+              $scope.newblacklist.addr = '地址未找到'
+            } else {
+              // $rootScope.flashMessage = 'Address Not Found';
+              $scope.newblacklist.addr = '地址未找到'
+            }
+          });
       }
     };
 
@@ -210,6 +226,7 @@ angular.module('insight.blacklists')
       })
       $scope.checked = [];
       $scope.bedit = true;
+      $scope.select_all = false;
     };
 
     $scope.checked = [];
@@ -740,7 +757,7 @@ angular.module('insight.history').controller('HistoryController',
       HistoryService.get({}, function (res) {
         if (res.code === 0) {
           $scope.histories = res.data;
-          console.log($scope.histories)
+          // console.log('historys:',$scope.histories)
         }
       });
     }
@@ -1978,7 +1995,7 @@ angular.module('insight.system')
 angular.module('insight.history')
     .factory('HistoryService',
       function ($resource, Service) {
-        console.log("Service:", Service.apiPrefix)
+        // console.log("Service:", Service.apiPrefix)
         return $resource(Service.apiPrefix + '/history/histories');
       });
 
